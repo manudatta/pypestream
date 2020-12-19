137e5a74b4a49343225c0f3c0299eee42466c4b6
Manu Datta <manu.datta@gmail.com>
2020-12-19 22:35:07 +0800

    init: created initial flask project structure

---
+++ lendingapp/__init__.py
@@ -0,0 +1,38 @@
+import os
+
+from flask import Flask
+
+######################################
+#### Application Factory Function ####
+######################################
+from lendingapp import db
+
+
+def create_app(config_filename='lendingapp.cfg'):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile(config_filename)
+    initialize_extensions(app)
+    #register_blueprints(app)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route("/hello")
+    def hello():
+        return "Hello, World!"
+
+    return app
+
+##########################
+#### Helper Functions ####
+##########################
+
+def initialize_extensions(app):
+    # Since the application instance is now created, pass it to each Flask
+    # extension instance to bind it to the Flask application instance (app)
+    db.init_app(app)
+
